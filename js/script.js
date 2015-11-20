cc0b0e736a2c6bf42f3f18393b231fe7205acdc9
Nick Gill <GILL@Gills-MacBook-Pro.local>
2015-11-19 21:27:43 -0600

    new changes

---
+++ js/script.js
@@ -0,0 +1,38 @@
+function employeeObj(firstName, lastName, employeeNum, title, reviewScore, salary) {
+  $('ul').append('<li>' + employeeObj.firstname + ', ' + employeeObj.lastname + ', ' + employeeObj.employeeNum + ', ' + employeeObj.title + ', ' + employeeObj.reviewScore + ', ' + employeeObj.salary + '</li>');
+};
+
+
+  var field_firstName = $('#firstName').val();
+  var field_lastName = $('#lastName').val();
+  var field_employeeNum = $('#employeeNum').val();
+  var field_title = $('#title').val();
+  var field_reviewScore = $('#reviewScore').val();
+  var field_salary = $('#salary').val();
+
+  employeeObj.firstname = field_firstName;
+  employeeObj.lastname = field_lastName;
+  employeeObj.employeeNum = field_employeeNum;
+  employeeObj.title = field_title;
+  employeeObj.reviewScore = field_reviewScore;
+  employeeObj.salary = field_salary;
+
+  var employeeTemplate = $('#entry-template').html();
+
+  // Compile the template
+  var theTemplate = Handlebars.compile(employeeTemplate);
+
+
+
+$('form').on('submit', function(event) {
+  event.preventDefault();
+
+var compiledHtml = theTemplate({employees: employeeObj()});
+
+ console.log(employeeObj);
+
+  // employeeArray.push(employeeObj);
+  //
+  // renderName(employeeObj);
+
+});
